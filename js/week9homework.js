6b4d4a1b9c9a132c02d4fe8f186adb994f920b38
mleo0010 <111230361+mleo0010@users.noreply.github.com>
2022-09-25 21:50:02 +1000

    Update week9homework.js

--- js/week9homework.js
+++ js/week9homework.js
@@ -1,4 +1,4 @@
-var vg_1 = "vizzes/data_vis2_proportional_symbols_map.json";
+var vg_1 = "https://raw.githubusercontent.com/mleo0010/FIT3179-DataVis2-ML/main/vizzes/data_vis2_proportional_symbols_map.json";
 
 vegaEmbed("#map_vis", vg_1).then(function(result) {
 
