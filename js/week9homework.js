b95c0353441d604c24976ae1316df1a5a63ab3ef
mleo0010 <111230361+mleo0010@users.noreply.github.com>
2022-10-09 23:30:36 +1100

    Update week9homework.js

--- js/week9homework.js
+++ js/week9homework.js
@@ -3,3 +3,10 @@ var vg_1 = "https://raw.githubusercontent.com/mleo0010/FIT3179-DataVis2-ML/main/
 vegaEmbed("#map_vis", vg_1).then(function(result) {
 
 }).catch(console.error);
+
+
+var vg_2 = "https://raw.githubusercontent.com/mleo0010/FIT3179-DataVis2-ML/main/vizzes/data_vis2_proportional_symbols_map.json";
+
+vegaEmbed("#player_wins_vis", vg_2).then(function(result) {
+
+}).catch(console.error);
